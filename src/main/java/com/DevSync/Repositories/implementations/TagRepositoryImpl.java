ed3ad443ec957e5eb641ef1d44cf99286466acc6
elkhailihamza <elkhailihamza331@gmail.com>
2024-10-01 21:45:37 +0100

    completed implementations

--- src/main/java/com/DevSync/Repositories/implementations/TagRepositoryImpl.java
+++ src/main/java/com/DevSync/Repositories/implementations/TagRepositoryImpl.java
@@ -2,32 +2,75 @@ package com.DevSync.Repositories.implementations;
 
 import com.DevSync.Entities.Tags;
 import com.DevSync.Repositories.TagRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class TagRepositoryImpl implements TagRepository {
+    private final SessionFactory sessionFactory;
+
+    public TagRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
-    public Tags findById(Long aLong) {
-        return null;
+    public Tags findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Tags.class, id);
+        }
     }
 
     @Override
     public List<Tags> fetchAll() {
-        return List.of();
+        try (Session session = sessionFactory.openSession()) {
+            Query<Tags> query = session.createQuery("FROM Tags", Tags.class);
+            return query.list();
+        }
     }
 
     @Override
     public void save(Tags entity) {
-
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+        }
     }
 
     @Override
     public void update(Tags entity) {
+        Transaction transaction = null;
 
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.merge(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+        }
     }
 
     @Override
     public void delete(Tags entity) {
+        Transaction transaction = null;
 
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            Query<Void> query = session.createQuery("DELETE FROM Tags WHERE id = :tagId", Void.class);
+            query.setParameter("tagId", entity.getId());
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+        }
     }
 }
--- src/main/java/com/DevSync/Repositories/implementations/TaskRepositoryImpl.java
+++ src/main/java/com/DevSync/Repositories/implementations/TaskRepositoryImpl.java
@@ -2,32 +2,73 @@ package com.DevSync.Repositories.implementations;
 
 import com.DevSync.Entities.Tasks;
 import com.DevSync.Repositories.TaskRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class TaskRepositoryImpl implements TaskRepository {
+    private final SessionFactory sessionFactory;
+
+    public TaskRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
-    public Tasks findById(Long aLong) {
-        return null;
+    public Tasks findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Tasks.class, id);
+        }
     }
 
     @Override
     public List<Tasks> fetchAll() {
-        return List.of();
+        try (Session session = sessionFactory.openSession()) {
+            Query<Tasks> query = session.createQuery("FROM Tasks", Tasks.class);
+            return query.list();
+        }
     }
 
     @Override
     public void save(Tasks entity) {
-
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+        }
     }
 
     @Override
     public void update(Tasks entity) {
-
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.merge(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+        }
     }
 
     @Override
     public void delete(Tasks entity) {
-
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            Query<Void> query = session.createQuery("DELETE FROM Tasks WHERE id = :taskId", Void.class);
+            query.setParameter("taskId", entity.getId());
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+        }
     }
 }
