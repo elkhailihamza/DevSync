b5db949c89785be0f3ec0de79042548d54931fe1
elkhailihamza <elkhailihamza331@gmail.com>
2024-10-07 14:53:38 +0100

    cleaned some code

--- src/main/java/com/DevSync/Servlets/User/UserServlet.java
+++ src/main/java/com/DevSync/Servlets/User/UserServlet.java
@@ -1,5 +1,8 @@
 package com.DevSync.Servlets.User;
 
+import com.DevSync.Controllers.UtilisateurController;
+import com.DevSync.Entities.Utilisateurs;
+import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,9 +11,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet("/users/*")
+@WebServlet("/users")
 public class UserServlet extends HttpServlet {
+    @Inject
+    protected UtilisateurController utilisateurController;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
@@ -24,14 +31,56 @@ public class UserServlet extends HttpServlet {
 
         String pathInfo = request.getPathInfo();
 
-        if (pathInfo == null || pathInfo.equals("/list")) {
-            request.getRequestDispatcher("/users/list").forward(request, response);
-        } else if (pathInfo.equals("/update")) {
-            request.getRequestDispatcher("/users/update").forward(request, response);
-        } else if (pathInfo.equals("/delete")) {
-            request.getRequestDispatcher("/users/delete").forward(request, response);
-        } else {
-            response.sendRedirect(request.getContextPath());
+        switch (pathInfo) {
+            case "/list":
+                request.setAttribute("contentPage", "/WEB-INF/Views/User/UserList.jsp");
+                List<Utilisateurs> users = utilisateurController.getAllUsers();
+                request.setAttribute("UserList", users);
+                break;
+            case "/update":
+
+                Utilisateurs selectedUser = (Utilisateurs) session.getAttribute("user");
+
+                if (selectedUser == null) {
+                    response.sendError(HttpServletResponse.SC_NOT_FOUND, "User not found");
+                    return;
+                }
+
+                request.setAttribute("contentPage", "/WEB-INF/Views/User/UserUpdate.jsp");
+                request.setAttribute("selectedUser", selectedUser);
+                break;
+            default:
+                response.sendRedirect(request.getContextPath());
+                break;
         }
+
+        request.getRequestDispatcher("/WEB-INF/app.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        long userId = Long.parseLong(request.getParameter("id"));
+
+        Utilisateurs user = Shared.assignValuesToUser(request);
+        Utilisateurs userForPass = utilisateurController.getCertainUser(userId);
+
+        user.setUser_pass(userForPass.getUser_pass());
+
+        utilisateurController.updateUser(user);
+        response.sendRedirect(request.getContextPath()+"/users");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String userId = request.getParameter("userId");
+
+        if (userId != null && !userId.isEmpty()) {
+            Utilisateurs user = new Utilisateurs();
+            user.setId(Long.parseLong(userId));
+            utilisateurController.deleteUser(user);
+            request.setAttribute("successMessage", "User successfully deleted!");
+        }
+
+        response.sendRedirect(request.getContextPath() + "/users");
     }
 }
